35874d8b64f37d05fed25b1fea4ae0e21330aead
avi-1227 <avinaashprasadd@gmail.com>
2023-06-08 01:22:43 +0530

    Register Category and Product Model

--- shopapp/models.py
+++ shopapp/models.py
@@ -29,7 +29,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200)
     image = models.ImageField(upload_to='products/%Y-%m-%d', blank=True)
     description = models.TextField(blank=True)
-    # always use DecimalField to store monetary amounts. By using the Decimal type, you will avoid fload rounding issues.
+    # always use DecimalField to store monetary amounts. By using the Decimal type, you will avoid float rounding issues.
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
