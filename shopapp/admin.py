60c4effe4845d227fa42dfaa71b1aab9a2f0465b
avi-1227 <avinaashprasadd@gmail.com>
2023-06-09 22:14:48 +0530

    Create and register Category and Product in admin

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -9,8 +9,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'available', 'created', 'update']
-    list_filter = ['available', 'created', 'update']
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug':('name',)}
-    
